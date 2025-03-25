c2222f6eb894533aac18ed719b24088674ceb8ce
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-25 16:33:02 +0100

    feat[cartContext]: remove logs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,6 @@ import Hero from '../components/Hero'
 const Home = () => {
   const { products } = useContext(ProductContext);
 
-  console.log(products);
 
   const filteredProducts = products.filter((item) => {
     return (
