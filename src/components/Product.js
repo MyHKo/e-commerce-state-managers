e396eb31688da50f69c8011e3f1654f47dfdd52d
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-21 15:31:42 +0200

    Redux: add redux to product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,13 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import { BsPlus, BsEyeFill } from "react-icons/bs";
+import {addToCart} from "../redux/actions/cartActions";
+import {useDispatch} from "react-redux";
 
-import { CartContext } from "../contexts/CartContext";
 
 const Product = ({ product }) => {
-  const { addToCart } = useContext(CartContext);
+  const dispatch = useDispatch();
 
   // destructure product
   const { id, image, category, title, price } = product;
@@ -25,7 +26,9 @@ const Product = ({ product }) => {
         </div>
         {/* buttons */}
         <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
-          <button onClick={() => addToCart(product, id)}>
+          <button onClick={() => {
+            dispatch(addToCart(product, id))
+          }}>
             <div className="flex justify-center items-center text-white w-12 h-12 bg-teal-500">
               <BsPlus className="text-3xl" />
             </div>
