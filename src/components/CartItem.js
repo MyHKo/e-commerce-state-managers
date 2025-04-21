3a512043022505abf4a7bc95c6b8219062442ec7
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-21 15:28:28 +0200

    Redux: add redux to cart component

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
 
-import { CartContext } from "../contexts/CartContext";
+import {useDispatch} from "react-redux";
+import {decreaseAmount, increaseAmount, removeFromCart} from "../redux/actions/cartActions";
 
 const CartItem = ({ item }) => {
-  const { removeFromCart, increaseAmount, decreaseAmount } = useContext(CartContext);
-  // destructure item
+  const dispatch = useDispatch();
   const { id, title, image, price, amount } = item;
 
   return (
@@ -29,7 +29,9 @@ const CartItem = ({ item }) => {
             </Link>
             {/* remove icon */}
             <div
-              onClick={() => removeFromCart(id)}
+              onClick={() => {
+                dispatch(removeFromCart(id))
+              }}
               className="text-xl cursor-pointer"
             >
               <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
@@ -38,13 +40,17 @@ const CartItem = ({ item }) => {
           <div className="flex gap-x-2 h-[36px] text-sm">
             {/* quantity */}
             <div className="flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium">
-              <div onClick={()=>decreaseAmount(id)} className="h-full flex-1 flex justify-center items-center cursor-pointer">
+              <div onClick={()=> {
+                dispatch(decreaseAmount(id))
+              }} className="h-full flex-1 flex justify-center items-center cursor-pointer">
                 <IoMdRemove />
               </div>
               <div className="h-full flex justify-center items-center px-2">
                 {amount}
               </div>
-              <div onClick={()=>increaseAmount(id)} className="h-full flex flex-1 justify-center items-center cursor-pointer">
+              <div onClick={()=> {
+                dispatch(increaseAmount(id))
+              }} className="h-full flex flex-1 justify-center items-center cursor-pointer">
                 <IoMdAdd />
               </div>
             </div>
