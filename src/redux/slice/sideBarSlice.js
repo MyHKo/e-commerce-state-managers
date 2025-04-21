999a7bd09fea120aff9b309f4406977d41ed2345
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-21 18:36:21 +0200

    ReduxTK: add sideBarSlice

--- src/redux/slice/sideBarSlice.js
+++ src/redux/slice/sideBarSlice.js
@@ -1,1 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    isOpen: false,
+}
+
+const sideBarSlice = createSlice({
+    name: "sideBar",
+    initialState,
+    reducers: {
+        toggleSidebar: (state) => {
+            state.isOpen = !state.isOpen
+        },
+        closeSidebar: (state) => {
+            state.isOpen = false
+        }
+    }
+})
