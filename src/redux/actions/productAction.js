ce6a009d16beeb455669af7b5a95b4f59c3fa7c2
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-21 14:51:07 +0200

    Redux: add product reducer

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,16 +1,14 @@
-import { FETCH_PRODUCTS_SUCCESS, FETCH_PRODUCTS_ERROR, FETCH_PRODUCTS_REQUEST} from "../constants";
+import { FETCH_PRODUCTS_SUCCESS, FETCH_PRODUCTS_REQUEST} from "../constants";
 
 export const fetchProducts = () => {
     return async (dispatch) => {
-        dispatch({type: FETCH_PRODUCTS_REQUEST})
-
         try {
             const response = fetch("https://fakestoreapi.com/products")
             const data = (await response).json()
 
             dispatch({type: FETCH_PRODUCTS_SUCCESS, payload: data})
         } catch (e) {
-            dispatch({type: FETCH_PRODUCTS_ERROR, e})
+            console.error(e)
         }
     }
 }
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,15 @@
+import { FETCH_PRODUCTS_SUCCESS } from "../constants";
+
+const initialState = {
+    products: [],
+}
+
+const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_PRODUCTS_SUCCESS:
+            return {
+                ...state,
+                products: action.payload
+            }
+    }
+}
