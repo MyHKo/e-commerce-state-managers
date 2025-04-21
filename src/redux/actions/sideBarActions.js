d1c738a13365ed90e141c25bf50ee02026c99811
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-21 09:32:24 +0200

    Redux: add sidebar actions

---
+++ src/redux/actions/sideBarActions.js
@@ -0,0 +1,9 @@
+import {CLOSE_SIDEBAR, OPEN_SIDEBAR} from "../constants";
+
+export const openSidebar = () => ({
+    type: OPEN_SIDEBAR,
+})
+
+export const closeSidebar = () => ({
+    type: CLOSE_SIDEBAR,
+})
