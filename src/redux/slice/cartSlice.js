2fbe1f10eb2f53a579e9230c45cb43ec7bf0a575
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-21 18:44:11 +0200

    ReduxTK: add cart slice

---
+++ src/redux/slice/cartSlice.js
@@ -0,0 +1,86 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    cart: [],
+    amount: 0,
+    total: 0
+}
+
+const getItemAmount = (cart) => {
+    return cart.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.amount;
+    }, 0);
+}
+
+const getItemTotal = (cart) => {
+    return cart.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.price * currentItem.amount;
+    }, 0);
+}
+
+const assignValues = (state, newCart) => {
+    state.cart = newCart;
+    state.amount = getItemAmount(newCart);
+    state.total = getItemTotal(newCart);
+}
+
+const cartSlice = createSlice({
+    name: "cartSlice",
+    initialState,
+    reducers: {
+
+        addToCart: (state, action) => {
+            const cart = state.cart
+            const newCart = []
+            const newProduct = {...action.product};
+            newProduct.amount = 1;
+            for(let i = 0; i < cart.length; i++) {
+                if(cart[i].id === action.id) {
+                    newProduct.amount = ++cart[i].amount;
+                }
+                else {
+                    newCart.push(cart[i]);
+                }
+            }
+            newCart.push(newProduct);
+            assignValues(state, newCart);
+        },
+
+        removeFromCart: (state, action) => {
+            const newCart = state.cart.filter((item) => item.id !== action.id)
+            assignValues(state, newCart);
+        },
+
+        increaseAmount: (state, action) => {
+            const cart = state.cart
+            const newCart = [];
+            for(let i = 0; i < cart.length; i++) {
+                if(cart[i].id === action.id) {
+                    newCart.push({...cart[i], amount: ++cart[i].amount});
+                }
+                else {
+                    newCart.push(cart[i]);
+                }
+            }
+
+            assignValues(state, newCart);
+        },
+
+        decreaseAmount: (state, action) => {
+            const cart = state.cart
+            const newCart = [];
+            for(let i = 0; i < cart.length; i++) {
+                if(cart[i].id === action.id) {
+                    if(cart[i].amount - 1 > 0) {
+                        newCart.push({...cart[i], amount: --cart[i].amount});
+                    }
+                }
+                else {
+                    newCart.push(cart[i]);
+                }
+            }
+
+            assignValues(state, newCart);
+        }
+    }
+})
