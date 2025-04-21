70887d97cad62415b052cf6d01a69b7b24a0a55c
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-21 14:57:10 +0200

    Redux: add combine reducers

---
+++ src/redux/combineReducers.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import productReducer from "./reducers/productReducer";
+import cartReducer from "./reducers/cartReducer";
+import sideBarReducer from "./reducers/sideBarReducer";
+
+export default combineReducers({
+    product: productReducer,
+    cart: cartReducer,
+    sideBart: sideBarReducer
+})
