db399f49de91eb5af72d4f57a4594bb5082027a1
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-04-21 15:44:32 +0200

    Redux: fix bug with fetching

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -3,8 +3,8 @@ import { FETCH_PRODUCTS_SUCCESS } from "../constants";
 export const fetchProducts = () => {
     return async (dispatch) => {
         try {
-            const response = fetch("https://fakestoreapi.com/products")
-            const data = (await response).json()
+            const response = await fetch("https://fakestoreapi.com/products")
+            const data = await response.json()
 
             dispatch({type: FETCH_PRODUCTS_SUCCESS, payload: data})
         } catch (e) {
